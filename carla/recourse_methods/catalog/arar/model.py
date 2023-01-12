79491cf59e84e1412c4b2e8181311c959527fce7
Eisbaer8 <eisbaer8@live.de>
2023-01-12 16:12:29 +0100

    Stylecheck ARAR

--- carla/recourse_methods/catalog/arar/model.py
+++ carla/recourse_methods/catalog/arar/model.py
@@ -1,27 +1,17 @@
-from typing import Dict, Optional, List
-
-import carla
-
-from carla.recourse_methods.api import RecourseMethod
-
-import torch
+from typing import Dict, List, Optional
 
 import numpy as np
-
-
-from carla import log
-
+import pandas as pd
+import torch
 from tqdm import tqdm
 
-import pandas as pd
-
+import carla
+from carla.recourse_methods.api import RecourseMethod
 from carla.recourse_methods.processing import (
     check_counterfactuals,
     merge_default_parameters,
+    reconstruct_encoding_constraints,
 )
-
-from carla.recourse_methods.processing import reconstruct_encoding_constraints
-
 
 
 class ARAR(RecourseMethod):
@@ -72,23 +62,25 @@ class ARAR(RecourseMethod):
         International Conference on Machine Learning. PMLR, 2022.
 
     """
+
     _DEFAULT_HYPERPARAMS = {
-        'lr': 0.1, 
-        'lambd_init': 1.0, 
-        'decay_rate': 0.9, 
-        'outer_iters': 100, 
-        'inner_iters': 50,
-        'inner_max_pgd': False,
-        'early_stop': False,
-        'binary_cat_features': True,
-        'y_target': 1,
-        'epsilon': 0.05, 
-        'robust': True,
-        'verbose': True 
+        "lr": 0.1,
+        "lambd_init": 1.0,
+        "decay_rate": 0.9,
+        "outer_iters": 100,
+        "inner_iters": 50,
+        "inner_max_pgd": False,
+        "early_stop": False,
+        "binary_cat_features": True,
+        "y_target": 1,
+        "epsilon": 0.05,
+        "robust": True,
+        "verbose": True,
     }
 
-
-    def __init__(self, mlmodel: carla.models.MLModel, hyperparams: Optional[Dict] = None):
+    def __init__(
+        self, mlmodel: carla.models.MLModel, hyperparams: Optional[Dict] = None
+    ):
         supported_backends = ["pytorch"]
         if mlmodel.backend not in supported_backends:
             raise ValueError(
@@ -98,11 +90,10 @@ class ARAR(RecourseMethod):
 
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
 
-
         checked_hyperparams = merge_default_parameters(
             hyperparams, self._DEFAULT_HYPERPARAMS
         )
-        
+
         self._lr = checked_hyperparams["lr"]
         self._lambd_init = checked_hyperparams["lambd_init"]
         self._decay_rate = checked_hyperparams["decay_rate"]
@@ -110,47 +101,61 @@ class ARAR(RecourseMethod):
         self._inner_iters = checked_hyperparams["inner_iters"]
         self._inner_max_pgd = checked_hyperparams["inner_max_pgd"]
         self._early_stop = checked_hyperparams["early_stop"]
-        self._bce_loss = torch.nn.BCEWithLogitsLoss(reduction='none')
+        self._bce_loss = torch.nn.BCEWithLogitsLoss(reduction="none")
         self._binary_cat_features = checked_hyperparams["binary_cat_features"]
         self._y_target = checked_hyperparams["y_target"]
         self._epsilon = checked_hyperparams["epsilon"]
         self._robust = checked_hyperparams["robust"]
         self._verbose = checked_hyperparams["verbose"]
-        
-        if self._y_target not in [0,1]:
+
+        if self._y_target not in [0, 1]:
             raise ValueError(
                 f"{self._y_target} is not a supported target class (0 or 1)"
             )
 
-    def _find_recourse(self, x: torch.Tensor, cat_feature_indices: List[int], bounds: torch.Tensor = None ):
+    def _find_recourse(
+        self,
+        x: torch.Tensor,
+        cat_feature_indices: List[int],
+        bounds: torch.Tensor = None,
+    ):
         """
         Find a recourse action (implementation of Algorithm 1 in the paper)
         Inputs:     x: torch.Tensor with shape (N, D), negatively classified instances for which to generate recourse
                     cat_features_indices: List[int], List of positions of categorical features in x.
-                    bounds: torch.Tensor with shape (N, D, 2),default: None containing the min and max interventions 
+                    bounds: torch.Tensor with shape (N, D, 2),default: None containing the min and max interventions
         Outputs:    actions: np.array with shape (N, D), recourse actions found
                     valid: np.array with shape (N, ), whether the corresponding recourse action is valid
                     cost: np.array with shape (N, ), cost of the recourse actions found (L1 norm)
                     cfs: np.array with shape (N, D), counterfactuals found (follow from x and actions)
         """
-        D = x.shape[1]
 
         x_og = x.clone()
 
-        x_pertb = torch.autograd.Variable(torch.zeros(x.shape, device = self.device), requires_grad=True)  # to calculate the adversarial
-                                                                                     # intervention on the features
+        x_pertb = torch.autograd.Variable(
+            torch.zeros(x.shape, device=self.device), requires_grad=True
+        )  # to calculate the adversarial
+        # intervention on the features
         ae_tol = 1e-4  # for early stopping
-        actions = torch.zeros(x.shape, device=self.device)  # store here valid recourse found so far
+        actions = torch.zeros(
+            x.shape, device=self.device
+        )  # store here valid recourse found so far
 
-        target_vec = torch.ones(x.shape[0], device=self.device) * self._y_target  # to feed into the BCE loss
+        target_vec = (
+            torch.ones(x.shape[0], device=self.device) * self._y_target
+        )  # to feed into the BCE loss
         if self._y_target == 0:
             target_vec_inv = torch.ones(x.shape[0], device=self.device)
         else:
             target_vec_inv = torch.zeros(x.shape[0], device=self.device)
-        unfinished = torch.ones(x.shape[0], device=self.device)  # instances for which recourse was not found so far
+        unfinished = torch.ones(
+            x.shape[0], device=self.device
+        )  # instances for which recourse was not found so far
 
         # Define variable for which to do gradient descent, which can be updated with optimizer
-        delta = torch.autograd.Variable(torch.zeros(x.shape, device=self.device), requires_grad=True)
+        delta = torch.autograd.Variable(
+            torch.zeros(x.shape, device=self.device), requires_grad=True
+        )
         optimizer = torch.optim.Adam([delta], self._lr)
 
         # Models the effect of the recourse intervention on the features
@@ -163,19 +168,29 @@ class ARAR(RecourseMethod):
 
         # Solve the first order approximation to the inner maximization problem
         def solve_first_order_approx(x_og, x_pertb, delta, target_vec):
-            x_adv = perturbation_model(x_og, x_pertb, delta.detach())  # x_pertb is 0, only to backprop
-            loss_x = torch.mean(self._bce_loss(self._mlmodel.predict(x_adv).squeeze(), target_vec))
+            x_adv = perturbation_model(
+                x_og, x_pertb, delta.detach()
+            )  # x_pertb is 0, only to backprop
+            loss_x = torch.mean(
+                self._bce_loss(self._mlmodel.predict(x_adv).squeeze(), target_vec)
+            )
             grad = torch.autograd.grad(loss_x, x_pertb, create_graph=False)[0]
-            #sometime the grad is zero therefore it is not possible to normalize it
+            # sometime the grad is zero therefore it is not possible to normalize it
             sum = torch.sum(grad, dim=-1)
-            grad[sum!=0] = grad [sum!=0]/ torch.linalg.norm(grad[sum!=0], dim=-1, keepdims=True) * self._epsilon
+            grad[sum != 0] = (
+                grad[sum != 0]
+                / torch.linalg.norm(grad[sum != 0], dim=-1, keepdims=True)
+                * self._epsilon
+            )
             return grad  # akin to FGSM attack
-
-
 
         lambd = self._lambd_init
         prev_batch_loss = np.inf  # for early stopping
-        pbar = tqdm(range(self._outer_iters)) if self._verbose else range(self._outer_iters)
+        pbar = (
+            tqdm(range(self._outer_iters))
+            if self._verbose
+            else range(self._outer_iters)
+        )
         for outer_iter in pbar:
             for inner_iter in range(self._inner_iters):
                 optimizer.zero_grad()
@@ -190,7 +205,14 @@ class ARAR(RecourseMethod):
 
                         for _ in range(10):
                             optimizer2.zero_grad()
-                            loss_pertb = torch.mean(self._bce_loss(self._mlmodel.predict(x_og + pertb + delta.detach()).squeeze(),target_vec_inv))
+                            loss_pertb = torch.mean(
+                                self._bce_loss(
+                                    self._mlmodel.predict(
+                                        x_og + pertb + delta.detach()
+                                    ).squeeze(),
+                                    target_vec_inv,
+                                )
+                            )
                             loss_pertb.backward()
                             optimizer2.step()
 
@@ -198,37 +220,61 @@ class ARAR(RecourseMethod):
                             with torch.no_grad():
                                 norm = torch.linalg.norm(pertb, dim=-1)
                                 too_large = norm > self._epsilon
-                                pertb[too_large] = pertb[too_large] / norm[too_large, None] * self._epsilon
+                                pertb[too_large] = (
+                                    pertb[too_large]
+                                    / norm[too_large, None]
+                                    * self._epsilon
+                                )
                             x_cf = x_og + pertb.detach() + delta
 
-                            x_cf = reconstruct_encoding_constraints(x_cf, cat_feature_indices, self._binary_cat_features)
+                            x_cf = reconstruct_encoding_constraints(
+                                x_cf, cat_feature_indices, self._binary_cat_features
+                            )
                     else:
                         x_cf = perturbation_model(x_og, pertb.detach(), delta)
-                        x_cf = reconstruct_encoding_constraints(x_cf, cat_feature_indices, self._binary_cat_features)
+                        x_cf = reconstruct_encoding_constraints(
+                            x_cf, cat_feature_indices, self._binary_cat_features
+                        )
                 else:
                     x_cf = recourse_model(x_og, delta)
-                    x_cf = reconstruct_encoding_constraints(x_cf, cat_feature_indices, self._binary_cat_features)
+                    x_cf = reconstruct_encoding_constraints(
+                        x_cf, cat_feature_indices, self._binary_cat_features
+                    )
 
                 with torch.no_grad():
                     # To continue optimazing, either the counterfactual or the adversarial counterfactual must be
                     # negatively classified
                     if self._y_target == 1:
-                        pre_unfinished_1 = self._mlmodel.predict(recourse_model(x_og, delta.detach())) <= 0.5
-                        pre_unfinished_2 = self._mlmodel.predict(x_cf) <= 0.5  # cf adversarial
+                        pre_unfinished_1 = (
+                            self._mlmodel.predict(recourse_model(x_og, delta.detach()))
+                            <= 0.5
+                        )
+                        pre_unfinished_2 = (
+                            self._mlmodel.predict(x_cf) <= 0.5
+                        )  # cf adversarial
                     elif self._y_target == 0:
-                        pre_unfinished_1 = self._mlmodel.predict(recourse_model(x_og, delta.detach())) >= 0.5
+                        pre_unfinished_1 = (
+                            self._mlmodel.predict(recourse_model(x_og, delta.detach()))
+                            >= 0.5
+                        )
                         pre_unfinished_2 = self._mlmodel.predict(x_cf) >= 0.5
 
-                    pre_unfinished = torch.logical_or(pre_unfinished_1, pre_unfinished_2)
-                    
+                    pre_unfinished = torch.logical_or(
+                        pre_unfinished_1, pre_unfinished_2
+                    )
+
                     # Add new solution to solutions
                     pre_unfinished = pre_unfinished.squeeze()
-                    new_solution = torch.logical_and(unfinished, torch.logical_not(pre_unfinished))
+                    new_solution = torch.logical_and(
+                        unfinished, torch.logical_not(pre_unfinished)
+                    )
                     actions[new_solution] = torch.clone(delta[new_solution].detach())
                     unfinished = torch.logical_and(pre_unfinished, unfinished)
 
                 # Compute loss
-                clf_loss = self._bce_loss(self._mlmodel.predict(x_cf).squeeze(), target_vec)
+                clf_loss = self._bce_loss(
+                    self._mlmodel.predict(x_cf).squeeze(), target_vec
+                )
                 l1_loss = torch.sum(torch.abs(delta), -1)
                 loss = clf_loss + lambd * l1_loss
 
@@ -241,9 +287,11 @@ class ARAR(RecourseMethod):
                 optimizer.step()
 
                 # Satisfy the constraints on the features, by projecting delta
-                if bounds:    
+                if bounds:
                     with torch.no_grad():
-                        delta[:] = torch.min(torch.max(delta, bounds[..., 0]), bounds[..., 1])
+                        delta[:] = torch.min(
+                            torch.max(delta, bounds[..., 0]), bounds[..., 1]
+                        )
 
                 # For early stopping
                 if self._early_stop and inner_iter % (self._inner_iters // 10) == 0:
@@ -254,7 +302,10 @@ class ARAR(RecourseMethod):
             lambd *= self._decay_rate
 
             if self._verbose:
-                pbar.set_description("Pct left: %.3f Lambda: %.4f" % (float(unfinished.sum()/x_cf.shape[0]), lambd))
+                pbar.set_description(
+                    "Pct left: %.3f Lambda: %.4f"
+                    % (float(unfinished.sum() / x_cf.shape[0]), lambd)
+                )
 
             # Get out of the loop if recourse was found for every individual
             if not torch.any(unfinished):
@@ -265,8 +316,9 @@ class ARAR(RecourseMethod):
         cost = torch.sum(torch.abs(actions), -1).detach().cpu().numpy()
         return actions.detach().cpu().numpy(), valid, cost, cfs
 
-
-    def get_counterfactuals(self, factuals: pd.DataFrame, bounds: torch.Tensor = None) -> pd.DataFrame:
+    def get_counterfactuals(
+        self, factuals: pd.DataFrame, bounds: torch.Tensor = None
+    ) -> pd.DataFrame:
         # This property is responsible to generate and output
         # encoded and scaled (i.e. transformed) counterfactual examples
         # as pandas DataFrames.
@@ -276,7 +328,7 @@ class ARAR(RecourseMethod):
         # It's expected that there is a single counterfactual per factual,
         # however in case a counterfactual cannot be found it should be NaN.
         factuals = self._mlmodel.get_ordered_features(factuals)
-        
+
         encoded_feature_names = self._mlmodel.data.encoder.get_feature_names(
             self._mlmodel.data.categorical
         )
@@ -287,10 +339,11 @@ class ARAR(RecourseMethod):
         x = torch.from_numpy(factuals.to_numpy().astype(np.float32)).to(self.device)
         actions, valid, cost, cfs = self._find_recourse(x, cat_features_indices, bounds)
         df_cfs = pd.DataFrame(cfs, columns=factuals.columns)
-        pred = self._mlmodel.predict(cfs)
         negative_label = 0
         if self._y_target == 0:
-            negative_label =1
-        df_cfs = check_counterfactuals(self._mlmodel, df_cfs, factuals.index, negative_label=negative_label)
+            negative_label = 1
+        df_cfs = check_counterfactuals(
+            self._mlmodel, df_cfs, factuals.index, negative_label=negative_label
+        )
         df_cfs = self._mlmodel.get_ordered_features(df_cfs)
         return df_cfs
